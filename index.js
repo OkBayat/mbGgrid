b3e192caa480f050e21d3fd9e2c0321afa190225
Mohammad Bayat <Ok.Bayat@hotmail.com>
2018-01-11 17:45:29 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -2,27 +2,27 @@
 const config = [
     {
         binding: 'name',
-        width: 100
-    },
-    {
-        binding: 'lname',
-        width: 150
-    },
-    {
-        binding: 'age',
         width: 50
     },
     {
-        binding: 'hight',
+        binding: 'lname',
         width: 60
     },
     {
+        binding: 'age',
+        width: 70
+    },
+    {
+        binding: 'hight',
+        width: 80
+    },
+    {
         binding: 'phone',
-        width: 150
+        width: 90
     },
     {
         binding: 'id',
-        width: 200
+        width: 100
     }
 ];
 const data = [];
@@ -33,47 +33,56 @@ for(let i = 0; i < 100; i++){
 
 const isFireFox = navigator.userAgent.toLowerCase().indexOf('firefox') > 1;
 
-
+let body;
+let table;
+let thead;
 let tbody;
+
 let order = {
     by: "hight",
     key: true
 }
 initGrid();
 function initGrid() {
-    const body = mb.select(".body");
+    body = mb.select(".body");
 
     body.remove("*");
 
-    const table = body.append("table")
-        .attr("border", 0)
-        .attr("cellpadding", 0)
-        .attr("cellspacing", 0);
+    table = body.append("div")
+        .attr("class", "table")
+        .style("grid-template-columns", function () {
+            const columns = [];
+            for (let i = 0, n = config.length; i < n; i++) {
+                columns.push(config[i].width + "px");
+            }
+            return columns.join(" ");
+        });
 
-    const thead = table.append("thead")
-        .selectAll("tr")
+    thead = table
+        .selectAll("div")
         .data([config])
         .enter()
-        .append("tr");
+        .append("div")
+        .attr("class", "thead");
 
-    tbody = table
-        .selectAll("tbody")
-        .data([data])
-        .enter()
-        .append("tbody");
 
-    thead
-        .append("td")
-        .attr("class", "side");
+    //thead
+    //    .append("div")
+    //    .attr("class", "side");
 
     thead
-        .selectAll("td:not(.side)")
+        .selectAll("div:not(.side)")
         .data(config)
         .enter()
-        .append("td")
-        .style("min-width", d => d.width + "px")
+        .append("div")
         .html(d => d.binding)
         .call(movementColumn())
+        .each(function(d) {
+            mb.watch(d, "width", (n) => {
+                const index = config.indexOf(mb.select(this).data());
+                gridTemplateColumnsUpdate(n, index);
+            });
+        })
         .on("click", function (d) {
             if (order.by === d.binding) {
                 order.key = !order.key;
@@ -99,91 +108,77 @@ function initGrid() {
         .attr("class", "resize-column")
         .call(resizeColumn());
 
-    mb.watch(order, "by", () => {
-        thead
-            .selectAll("td")
-            .classed("order", d => d.binding === order.by);
-    });
-    mb.watch(order, "key", (n) => {
-        thead
-            .select(".order")
-            .classed("desc", !n);
-    });
+    //mb.watch(order, "by", () => {
+    //    thead
+    //        .selectAll("td")
+    //        .classed("order", d => d.binding === order.by);
+    //});
+    //mb.watch(order, "key", (n) => {
+    //    thead
+    //        .select(".order")
+    //        .classed("desc", !n);
+    //});
 
-    mb.select(".body tbody")
-        .selectAll("tr")
+    tbody = table
+        .selectAll(".grid-row:not(.thead)")
         .data(data)
         .enter()
-        .append("tr");
+        .append("div");
 
-    mb.select(".body")
-        .on("scroll",
-        function () {
-            mb.select(this)
-                .select("thead")
-                .style("transform", `translate(0, ${this.scrollTop}px)`);
+    body.on("scroll", function () {
+        const
+            bodyNode = body.node(),
+            bodyOffset = bodyNode.getBoundingClientRect(),
+            translateLeft = isFireFox
+                ? bodyNode.scrollLeft
+                : -(bodyNode.scrollWidth - bodyNode.clientWidth - bodyNode.scrollLeft);
 
-            const body = mb.select(".body").node();
-            let translateLeft;
-            if (isFireFox) {
-                translateLeft = body.scrollLeft;
-            } else {
-                translateLeft = -(body.scrollWidth - body.clientWidth - body.scrollLeft);
-            }
-            thead
-                .select("td")
-                .style("transform", function () { return `translate(${translateLeft}px, 0)` });
 
-            mb.select(this)
-                .select("tbody")
-                .selectAll("tr")
-                .watch(function(d, i) {
-                    const body = mb.select(".body").node().getBoundingClientRect();
-                    const row = this.getBoundingClientRect();
+        thead.style("transform", `translate(0, ${this.scrollTop}px)`);
+        thead.select("div")
+            .style("transform", `translate(${translateLeft}px, 0)`);
 
-                    if (body.top < row.bottom &&
-                        body.bottom > row.top) {
+        tbody.each(function(d) {
+                const row = this.getBoundingClientRect();
+
+                if (bodyOffset.top < row.bottom &&
+                    bodyOffset.bottom > row.top) {
+
+                    //mb.select(this)
+                    //    .selectAll(".side")
+                    //    .data([1])
+                    //    .enter()
+                    //    .append("div")
+                    //    .on("click",
+                    //        function() {
+                    //            const selfRow = mb.select(this)
+                    //                .parent()
+                    //                .node();
+
+                    //            mb.select(this)
+                    //                .parent()
+                    //                .parent()
+                    //                .insertAfter("tr", selfRow)
+                    //                .attr("class", "cardx")
+                    //                .append("tr");
+                    //        })
+                    //    .attr("class", "side");
+
+                    //if (this.className !== "cardx") {
+                        enterCell.call(this, d)
+                            .append("div")
+                            .html(d => d);
 
                         mb.select(this)
-                            .selectAll(".side")
-                            .data([1])
-                            .enter()
-                            .append("td")
-                            .on("click", function() {
-                                const selfRow = mb.select(this)
-                                    .parent()
-                                    .node();
-
-                                mb.select(this)
-                                    .parent()
-                                    .parent()
-                                    .insertAfter("tr", selfRow)
-                                    .attr("class", "cardx")
-                                    .append("tr");
-                            })
-                            .attr("class", "side");
-
-                        if (this.className !== "cardx") {
-                            enterCell.call(this, d)
-                                .append("td")
-                                .html(d => d);
-
-                            mb.select(this)
-                                .select("td")
-                                .style("transform", `translate(${translateLeft}px, 0)`);
-                        }
-
-                    } else {
-                        mb.select(this)
-                            .remove("*");
-                    }
-                })
-                //.watch(function(d) {
-                //    enterCell.call(this, d)
-                //        .html(d => d);
-                //});
-
-        });
+                            .select("div")
+                            .style("transform", `translate(${translateLeft}px, 0)`);
+                    //}
+                } else {
+                    mb.select(this)
+                        .remove("*");
+                }
+            });
+    });
 
     mb.select(".body").node().scrollTop = 1;
     mb.select(".body").node().scrollTop = 0;
@@ -196,33 +191,48 @@ function enterCell(d) {
     }
     
     return mb.select(this)
-        .selectAll("td:not(.side)")
+        .selectAll("div:not(.side)")
         .data(data)
         .enter();
+}
+function gridTemplateColumnsUpdate(d, i) {
+    const temp = table.node().style.gridTemplateColumns.split(" ");
+    temp[i] = d + "px";
+
+    table.style("grid-template-columns", temp.join(" "));
 }
 function movementColumn() {
     return mb.drag()
         .on("end", function (d, i) {
-            const
-                headerData = mb.select(this).parent().data(),
-                hoveredData = mb.select(mb.event.target).data(),
-                hoveredIndex = headerData.indexOf(hoveredData);
+            const hoveredData = mb.select(mb.event.target).data();
+            const elm = d;
+            let fromIndex = config.indexOf(d);
+            let toIndex = config.indexOf(hoveredData);
+            config.splice(fromIndex, 1);
 
-            headerData.splice(i, 1);
-            headerData.splice(hoveredIndex, 0, d);
-
+            if (fromIndex > toIndex + 1) {
+                config.splice(toIndex + 1, 0, elm);
+            } else {
+                config.splice(toIndex, 0, elm);
+            }
             mb.select(this)
                 .parent()
-                .insertAfter(this, hoveredIndex);
+                .insertAfter(this, toIndex);
+
+            ////-----------
+            const columns = [];
+            for (let j = 0, n = config.length; j < n; j++) {
+                columns.push(config[j].width + "px");
+            }
+            table.style("grid-template-columns", columns.join(" "));
+            //-----------
 
 
-            tbody
-                .selectAll("tr")
-                .child(i)
+            tbody.child(i)
                 .each(function () {
                     mb.select(this)
                         .parent()
-                        .insertAfter(this, hoveredIndex);
+                        .insertAfter(this, toIndex);
                 });
         });
 }
@@ -233,16 +243,11 @@ function resizeColumn() {
             this.x = mb.event.clientX;
             this.w = d.width;
         })
-        .on("drag", function () {
-            mb.select(this)
-                .parent()
-                .style("min-width", d => {
-                    d.width = this.w + this.x - mb.event.clientX;
-                    return d.width + "px";
-                });
+        .on("drag", function (d) {
+            d.width = this.w + this.x - mb.event.clientX;
         })
         .on("end", function () {
             this.x = null;
             this.w = null;
-        })
+        });
 }
